a2218a7107cbbb4c5dfbfc541eef9b38682968fb
AbrahamMougle <abrahammougle76@gmail.com>
2025-05-31 04:40:18 +0200

    juste en adaptant l'interface quand on gagne

--- react-tailwindcss/src/component/Square.tsx
+++ react-tailwindcss/src/component/Square.tsx
@@ -2,12 +2,14 @@
 interface SquareProps {
   value: string;
   onSquareClick?: () => void;
+  highlight?: boolean;
 }
 
-export default function Square({ value, onSquareClick }: SquareProps) {
+export default function Square({ value, onSquareClick, highlight }: SquareProps) {
   return (
     <button
-      className="border w-40 h-12 p-1 font-bold text-3xl border-gray-300 min-h-20 rounded-md flex items-center justify-center"
+      className={`border w-40 h-12 p-1 font-bold text-3xl border-gray-300 min-h-20 rounded-md flex items-center justify-center
+        ${highlight ? "bg-green-300" : "bg-red-300"}`}
       onClick={onSquareClick}
       type="button"
     >
