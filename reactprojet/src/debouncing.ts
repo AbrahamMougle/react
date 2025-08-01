41204385ef50f58034e005cb4a3dd6e600f55253
AbrahamMougle <abrahammougle76@gmail.com>
2025-08-01 15:54:19 +0200

    bon typage

---
+++ reactprojet/src/debouncing.ts
@@ -0,0 +1,17 @@
+"use client"
+export default function debounce<F extends (...args: any[]) => void>(
+  func: F,
+  delay: number
+): (...args: Parameters<F>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  return (...args: Parameters<F>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      timeoutId = null;
+    }, delay);
+  };
+}
